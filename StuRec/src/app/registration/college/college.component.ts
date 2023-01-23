d69ea03e3e9cd0393fd6bb3529c228385489d5b5
Somdev Mondal <Somdev.Mondal@rencata.com>
2023-01-23 18:34:10 +0530

    Corrected Institute Registration

--- StuRec/src/app/registration/college/college.component.ts
+++ StuRec/src/app/registration/college/college.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-college',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./college.component.css']
 })
 export class CollegeComponent {
+  regDetails = new FormGroup({
+    instituteName: new FormControl(''),
+    userEmail: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+    address: new FormControl(''),
+    city: new FormControl(''),
+    state: new FormControl(''),
+    zip: new FormControl(''),
+    phone: new FormControl(''),
+    instituteURL: new FormControl('')
+  });
 
+  onRegister() {
+    console.log(this.regDetails.value);
+  }
 }
