03d43951c00382f640aa2f653c252570025d25d3
Somdev Mondal <Somdev.Mondal@rencata.com>
2023-02-02 00:11:05 +0530

    Corporate Update and delete

--- node-rest-StuRec/api/routes/corporates.js
+++ node-rest-StuRec/api/routes/corporates.js
@@ -40,8 +40,9 @@ async function getCorporate(id) {
         const db = client.db(database);
         const collection = db.collection('corporate');
         return collection.findOne({_id: new ObjectId(id)});
-    } catch (error) {
-        console.log(error);
+    }
+    catch (error) {
+        throw error;
     }
 }
 
@@ -62,36 +63,101 @@ router.get('/:corporateId', (req,res,next) => {
     });
 });
 
+async function addCorporate(data) {
+    try {
+        await client.connect();
+        const db = client.db(database);
+        const collection = db.collection('corporate'); 
+        return await collection.insertOne(data);
+    }
+    catch (error) {
+        throw error;
+    }
+}
+
 router.put('/', (req,res,next) => {
     const inputJsonData = {
         orgName: req.body.orgName,
         firstName: req.body.firstName,
         lastName: req.body.lastName
     };
-    res.status(200).json({
-        message: 'Handel PUT request for corporates',
-        data: inputJsonData
-    });
+
+    addCorporate(inputJsonData).then(dt => { 
+        res.status(200).json({
+            message: 'Handel PUT request for corporates',
+            data: dt
+        });
+    }).catch(err => {
+        res.status(500).json({
+            message: 'Handel PUT request for corporates. Has error',
+            data: err
+        });
+    }).finally(() => { 
+        client.close(); 
+    });   
 });
 
+async function updateCorporate(id,data) {
+    try {
+        await client.connect();
+        const db = client.db(database);
+        const collection = db.collection('corporate'); 
+        return await collection.updateOne({_id: new ObjectId(id)},{ $set: data}, {upsert: false});
+    }
+    catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
 router.post('/', (req,res,next) => {
+    const id = req.body.corporateId;
     const inputJsonData = {
-        corporateId: req.body.corporateId,
         orgName: req.body.orgName,
         firstName: req.body.firstName,
         lastName: req.body.lastName
     };
-    res.status(201).json({
-        message: 'Handel POST request for corporates',
-        data: inputJsonData
+    updateCorporate(id,inputJsonData).then((dt) => {
+        res.status(201).json({
+            message: 'Handel POST request for corporate',
+            data: dt
+        });
+    }).catch((err) => {
+        res.status(500).json({
+            message: 'Handel Post request for corporates. Has error',
+            data: err
+        });
+    }).finally(() => {
+        client.close(); 
     });
 });
 
+async function deleteCorporate(id) {
+    try {
+        await client.connect();
+        const db = client.db(database);
+        const collection = db.collection('corporate'); 
+        return await collection.deleteOne({ _id: new ObjectId(id) });
+    }
+    catch(error) {
+        throw error;
+    }
+}
+
 router.delete('/:corporateId', (req,res,next) => {
     const id = req.params.corporateId;
-    res.status(200).json({
-        message: 'Handel DELETE request for corporates',
-        id: id
+    deleteCorporate(id).then((dt) => {
+        res.status(200).json({
+            message: 'Handel DELETE request for corporate',
+            id: dt
+        });
+    }).catch((err) => {
+        res.status(500).json({
+            message: 'Handel DELETE request for corporate. Has error',
+            data: err
+        });
+    }).finally(() => {
+        client.close(); 
     });
 });
 
